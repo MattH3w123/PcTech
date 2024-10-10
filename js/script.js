749407e2032cb4c75b94fdb649ce6d7d8e836400
MattH3w123 <matirz101@gmail.com>
2024-10-10 20:10:51 +0200

    Add new files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+gsap.registerPlugin(ScrollTrigger);
+
+    const images = gsap.utils.toArray('#img-fk');
+
+    const showDemo = () => {
+      document.body.style.overflow = 'auto';
+      document.scrollingElement.scrollTo(0, 0);
+      
+      gsap.utils.toArray('#fk-gallery .gallery').forEach((section, index) => {
+        const w = section.querySelector('.wrapper');
+        const [x, xEnd] = (index % 2) ? ['100%', (w.scrollWidth - section.offsetWidth) * -1] : [w.scrollWidth * -1, 0];
+        gsap.fromTo(w, { x }, {
+          x: xEnd,
+          scrollTrigger: { 
+            trigger: section, 
+            scrub: 0.5 
+          }
+        });
+      });
+    }
+
+    imagesLoaded(images).on('always', showDemo);
